499e0cdaa7547ead113b74e5ceebfb9bb09e3831
Bintang Tobing <bintangtobing@theluxeweek.com>
2024-08-22 22:03:15 +0700

    update: bash

--- scripts/create_symlink.sh
+++ scripts/create_symlink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a symlink for public_html pointing to the public directory in your repo
-ln -sfn /home/patunganyuk/repo/patungan-lp/public /home/patunganyuk/public_html
+ln -sfn /home/patunganyuk/repo/patungan-lp/public/ /home/patunganyuk/public_html/
 
 # Set permissions for the symlink target if needed
 find /home/patunganyuk/public_html -type d -exec chmod 755 {} \;
