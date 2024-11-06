ddc9ddd8bdd6a7e8f312bd1f6062caa9568bc09c
Bintang Tobing <hello@bintangtobing.com>
2024-11-07 03:25:46 +0700

    update: pixel code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,12 +58,12 @@
       s.parentNode.insertBefore(t,s)
     }(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '508269598487802');
+    fbq('init', '473988542365157');
     fbq('track', 'PageView');
     </script>
     <noscript>
         <img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=508269598487802&ev=PageView&noscript=1" />
+            src="https://www.facebook.com/tr?id=473988542365157&ev=PageView&noscript=1" />
     </noscript>
     <!-- End Meta Pixel Code -->
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
